90fd8490c8ba715058d693e540ca79d84b45b527
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-02-11 01:50:33 +0200

    added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    const { good, neutral, bad } = state;
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+        <button>good</button>
+        <button>neutral</button>
+        <button>bad</button>
+        <h2>Statistics</h2>
+        <p>Good: </p>
+        <p>Neutral: </p>
+        <p>Bad: </p>
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,14 @@
+export function FeedbackOptions() {
+  return (
+    <>
+      <h2></h2>
+      <button></button>
+      <button></button>
+      <button></button>
+      <h2></h2>
+      <p></p>
+      <p></p>
+      <p></p>
+    </>
+  );
+}
